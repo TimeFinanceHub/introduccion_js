b031fee59517afe0181a5f825ce6b8ce1a822868
Ramiro G <mostlyphpsoftware@gmail.com>
2025-03-08 08:49:39 -0700

    menu nav

---
+++ extra/unirObjetos.js
@@ -0,0 +1,9 @@
+const objeto_1 = {propiedad_1 : "valor 1"}
+
+const objeto_2 = {propiedad_2 : "valor 2"}
+
+console.log(objeto_1,objeto_2);
+
+const objeto_3 = {...objeto_1,...objeto_2};
+
+console.log(objeto_3);
